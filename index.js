8ef80790ac9552f9e926c4d2a7b00ccd4bdab9fd
emmanuelleperra <emmanuelleperra.nd@gmail.com>
2026-02-15 18:34:24 +0100

    Added basic form handling + typewriter pluggin

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+function generateIdea(event) {
+    event.preventDefault();
+
+    new Typewriter('#book-idea', {
+        strings: "We are the ants",
+        autoStart: true,
+    });
+}
+
+
 let bookGeneratorForm = document.querySelector("#book-idea-form");
 bookGeneratorForm.addEventListener("submit", generateIdea);
